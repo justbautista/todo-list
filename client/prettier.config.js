8b80fa623f57ce5b316a227f869aef6d0c3e1600
justin <justinbautista923@gmail.com>
2023-10-12 10:18:01 -0400

    configured

---
+++ client/prettier.config.js
@@ -0,0 +1,26 @@
+const config = {
+	arrowParens: "always",
+	bracketSpacing: true,
+	endOfLine: "lf",
+	htmlWhitespaceSensitivity: "css",
+	insertPragma: false,
+	singleAttributePerLine: false,
+	bracketSameLine: false,
+	jsxBracketSameLine: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: "preserve",
+	quoteProps: "preserve",
+	requirePragma: false,
+	semi: false,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "es5",
+	useTabs: true,
+	embeddedLanguageFormatting: "auto",
+	vueIndentScriptAndStyle: false,
+	parser: "babel",
+	plugins: ["prettier-plugin-tailwindcss"],
+}
+
+export default config
---
+++ client/src/pages/TestPage.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function TestPage() {
+	return <div className="mx-auto bg-red-500 text-white">TestPage</div>
+}
